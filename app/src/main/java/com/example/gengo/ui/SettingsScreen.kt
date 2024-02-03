36464bc3977f8d542996b13d70ce9cf59aef8137
Wadim X. Janikowski <wadim.janikowski@gmail.com>
2024-02-03 16:05:32 +0100

    Modify the theme mode setting appearance

--- app/src/main/java/com/example/gengo/ui/SettingsScreen.kt
+++ app/src/main/java/com/example/gengo/ui/SettingsScreen.kt
@@ -2,8 +2,11 @@ package com.example.gengo.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +17,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.example.gengo.R
 
 @Composable
 fun SettingsScreen(
@@ -34,7 +40,15 @@ fun SettingsScreen(
             ),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Text("☀️")
+        Icon(
+            painter = painterResource(id = R.drawable.nightlight),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onBackground,
+        )
+        Text(
+            text = stringResource(R.string.dark_mode),
+        )
+        Spacer(Modifier.weight(1f))
         Switch(
             checked = theme,
             onCheckedChange = {
@@ -42,7 +56,6 @@ fun SettingsScreen(
                 onThemeSwitch()
             }
         )
-        Text("🌘")
     }
 }
 
